0aef2bdd56e0d0acbd6a213e9e924fc48289e9a7
Begana <begana.contact@gmail.com>
2024-10-08 18:31:49 +0200

    create random heading quote

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,21 @@
-import logoImg from '../assets/logo.png';
+import logoImg from "../assets/logo.png";
+
+import { quotes } from "../data.js";
+
+function generateRandomQuotes(quotes) {
+  const index = Math.floor(Math.random() * quotes.length);
+  return quotes[index];
+}
 
 export default function Header() {
-  const date = new Date().toLocaleDateString('de-DE');
+  const date = new Date().toLocaleDateString("de-DE");
 
-  const randomIndex = Math.floor(Math.random() * 10);
+  const randomQuote = generateRandomQuotes(quotes);
 
   return (
     <header>
-      <img src={logoImg} alt='a hand-drawing of burning elmo' />
-      <h1>Cried All?</h1>
+      <img src={logoImg} alt="a hand-drawing of burning elmo" />
+      <h1>{randomQuote}</h1>
       <h1>Now Get Back To Work!</h1>
       <h2>Good morning, Begana! Today is : {date}</h2>
     </header>
