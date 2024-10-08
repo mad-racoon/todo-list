c69a6bce42d7ba7fa0f9691ad8db0c29006ce681
begana choi <begana.contact@gmail.com>
2024-10-08 18:16:57 +0200

    create data

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,6 +3,8 @@ import logoImg from '../assets/logo.png';
 export default function Header() {
   const date = new Date().toLocaleDateString('de-DE');
 
+  const randomIndex = Math.floor(Math.random() * 10);
+
   return (
     <header>
       <img src={logoImg} alt='a hand-drawing of burning elmo' />
---
+++ src/data.js
@@ -0,0 +1,7 @@
+export const quotes = [
+  'There is no time to be sad!',
+  'Cried all?',
+  "Don't forget you are that bitch.",
+  'Nothing can stop you except yourself.',
+  "Don't let them ruin your day!",
+];
