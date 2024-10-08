ba89b188ebfc6080e5d917d2ea9f999a6e206d96
Begana <begana.contact@gmail.com>
2024-10-08 18:48:45 +0200

    create label in form

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,17 +1,25 @@
 export default function Form() {
   return (
-    <form action=''>
+    <form action="">
       <p>
-        <label htmlFor=''>Task</label>
-        <input type='text' placeholder='Enter a name of task' />
+        <label htmlFor="">Task</label>
+        <input type="text" placeholder="Enter a name of task" />
       </p>
       <p>
-        <label htmlFor=''>Description</label>
+        <label htmlFor="">Description</label>
         <textarea />
       </p>
       <p>
-        <label htmlFor=''>due date</label>
-        <input type='date' />
+        <label htmlFor="">Due Date</label>
+        <input type="date" />
+      </p>
+      <p>
+        <label htmlFor="">Select Label</label>
+        <select name="label" id="label">
+          <option value="green">green</option>
+          <option value="yellow">yellow</option>
+          <option value="red">red</option>
+        </select>
       </p>
     </form>
   );
