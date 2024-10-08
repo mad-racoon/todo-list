1afa79f8f7fe0a48228bc228494dc3360ddb8689
Begana <begana.contact@gmail.com>
2024-10-08 19:01:41 +0200

    add onClick feature on button

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,24 @@
-import Header from './components/Header.jsx';
-import Button from './components/Button.jsx';
-import Form from './components/Form.jsx';
+import { useState } from "react";
+
+import Header from "./components/Header.jsx";
+import Button from "./components/Button.jsx";
+import Form from "./components/Form.jsx";
 
 function App() {
+  const [addTask, setAddTask] = useState(false);
+
+  function handleClick() {
+    setAddTask((prevState) => !prevState);
+  }
+
   return (
     <>
       <Header />
       <main>
-        <Button />
-        <Form />
+        <Button onClick={handleClick}>
+          {addTask ? "Cancel" : "Add New Task"}
+        </Button>
+        {addTask && <Form />}
       </main>
     </>
   );
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,3 +1,3 @@
-export default function Button() {
-  return <button>Add Task</button>;
+export default function Button({ children, ...props }) {
+  return <button {...props}>{children}</button>;
 }
